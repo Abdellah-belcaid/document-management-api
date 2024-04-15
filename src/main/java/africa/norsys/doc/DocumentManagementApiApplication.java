7e25adc1bc4d9f5a48e8123bf294588cf7532438
belcaid <belcaid.abdellah2001@gmail.com>
2024-04-15 10:12:26 +0100

    first commit

---
+++ src/main/java/africa/norsys/doc/DocumentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package africa.norsys.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/africa/norsys/doc/DocumentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.norsys.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
