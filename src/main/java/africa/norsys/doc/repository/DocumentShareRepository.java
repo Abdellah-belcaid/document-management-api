53219edaded5dc71653be926a2e1c658d2da5fe4
naziksafi <nazikessafi2001@gmail.com>
2024-04-19 14:15:18 +0200

    file shared

--- src/main/java/africa/norsys/doc/repository/DocumentShareRepository.java
+++ src/main/java/africa/norsys/doc/repository/DocumentShareRepository.java
@@ -1,13 +1,19 @@
 package africa.norsys.doc.repository;
 
+import africa.norsys.doc.dto.UserPermissionDto;
 import africa.norsys.doc.entity.Document;
 import africa.norsys.doc.entity.DocumentShare;
 import africa.norsys.doc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DocumentShareRepository extends JpaRepository<DocumentShare, UUID> {
     Optional<DocumentShare> findByDocumentAndUser(Document document, User user);
+    @Query("SELECT new africa.norsys.doc.dto.UserPermissionDto(ds.user, ds.permission) FROM DocumentShare ds WHERE ds.document.id = :documentId")
+    List<UserPermissionDto> findUsersAndPermissionsByDocumentId(UUID documentId);
+
 }
